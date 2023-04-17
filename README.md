a83a6d77f03b41b81720e05d8347ac8d7706817b
YanaLiz <lapa.270519@gmail.com>
2023-04-17 23:18:20 +0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # node.js-CLI
+Увесь список контактів в таблиці:
+https://monosnap.com/file/Cc7dcJd563JiJCxplD9DAudl44IpuG ,
+отримання контакта по id:
+https://monosnap.com/file/5XTEREBvAjfDFDwWmMgTYk49Y8w36Q ,
+додавання контакта:
+https://monosnap.com/file/PrNfoF1Rpps2CdG2cXXYzllkM3vCjC ,
+видалення конткта:
+https://monosnap.com/file/2dACb8DNudLOgsRgkSSURegkPzXuZ9
--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-import nodemon from "nodemon";
-import path from "path";
+const contacts = require("./contacts");
+const argv = require("yargs").argv;
 
-console.log(path)
+
+const invokeAction = async ({action, id, name, email, phone}) => {
+  switch (action) {
+    case "list":
+      const contactsList = await contacts.listContacts();
+      return console.table(contactsList);
+
+    case "get":
+      const contact = await contacts.getContactById(id);
+      return console.log(contact);
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+
+    case "remove":
+      const contactRemove = await contacts.removeContact(id);
+      return console.log(contactRemove);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
